4c5bab42da6dfad5b47778ebc3f8984b9f869c7a
mikl-k <mikl-k@narod.ru>
2023-11-06 19:13:34 +0300

    1.5. Simple Spring Boot microservice with localhost:8080 Hello world

---
+++ src/test/java/SkillBox/com/users/UsersApplicationTests.java
@@ -0,0 +1,13 @@
+package SkillBox.com.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
