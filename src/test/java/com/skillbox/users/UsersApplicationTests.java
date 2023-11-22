6a63eee4dd2f6ca6ec3eed2b3225513ab005fd03
mikl-k <mikl-k@narod.ru>
2023-11-22 21:47:14 +0300

    mfvanek fix

--- src/test/java/com/skillbox/users/UsersApplicationTests.java
+++ src/test/java/com/skillbox/users/UsersApplicationTests.java
@@ -2,12 +2,16 @@ package com.skillbox.users;
 
 import com.skillbox.users.support.PostgreSQLInitializer;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
 @ActiveProfiles("test")
-class UsersApplicationTests extends PostgreSQLInitializer {
+@AutoConfigureMockMvc
+@ContextConfiguration(initializers = PostgreSQLInitializer.class)
+class UsersApplicationTests {
 
 	@Test
 	void contextLoads() {
--- src/test/java/com/skillbox/users/support/PostgreSQLInitializer.java
+++ src/test/java/com/skillbox/users/support/PostgreSQLInitializer.java
@@ -3,14 +3,10 @@ package com.skillbox.users.support;
 import org.springframework.boot.test.util.TestPropertyValues;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.ContextConfiguration;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.utility.DockerImageName;
 
-@ActiveProfiles("test")
-@ContextConfiguration(initializers = PostgreSQLInitializer.class)
 public class PostgreSQLInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
 
     private static final DockerImageName IMAGE = DockerImageName.parse("postgres:16.0-alpine");
@@ -20,15 +16,14 @@ public class PostgreSQLInitializer implements ApplicationContextInitializer<Conf
             .withDatabaseName("users")
             .waitingFor(Wait.forListeningPort());
 
+
     @Override
     public void initialize(final ConfigurableApplicationContext applicationContext) {
         POSTGRE_SQL_CONTAINER.start();
         TestPropertyValues.of(
                 "spring.datasource.url=" + POSTGRE_SQL_CONTAINER.getJdbcUrl(),
                 "spring.datasource.username=" + POSTGRE_SQL_CONTAINER.getUsername(),
-                "spring.datasource.password=" + POSTGRE_SQL_CONTAINER.getPassword(),
-                "flyway.user=", POSTGRE_SQL_CONTAINER.getUsername(),
-                "flyway.password=", POSTGRE_SQL_CONTAINER.getPassword()
+                "spring.datasource.password=" + POSTGRE_SQL_CONTAINER.getPassword()
         ).applyTo(applicationContext.getEnvironment());
     }
 }
